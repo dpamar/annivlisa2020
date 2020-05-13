16cd1d353a26984fcb6506f289d250bb65df8dcc
David AMAR <david@MacBook-Air-de-David.local>
2020-05-13 17:16:56 +0200

    All

--- places.js
+++ places.js
@@ -1,3 +1,13 @@
 var places = [
+    [45.180597, 5.722314, 'Entrez donc...'],
+    [45.180639, 5.722282, 'Lavez vous les mains d\'abord'],
+    [45.180659, 5.722252, 'Au milieu'],
+    [45.180658, 5.722257, 'La pièce d\'après'],
+    [45.180639, 5.722275, 'Faites demi-tour'],
+    [45.180608, 5.72228, 'Ouvrez voir'],
+    [45.180594, 5.722293, 'Un autre placard ?'],
+    [45.180594, 5.722293, 'Retour vers l\'entrée'],
+    [45.180594, 5.722293, 'La grande pièce...']
 ];
+//Resume game designer URL: https://dpamar.github.io/annivlisa2020/generator.html?places=W1s0NS4xODA1OTcsIDUuNzIyMzE0LCAnRW50cmV6IGRvbmMuLi4nXSxbNDUuMTgwNjM5LCA1LjcyMjI4MiwgJ0xhdmV6IHZvdXMgbGVzIG1haW5zIGRcJ2Fib3JkJ10sWzQ1LjE4MDY1OSwgNS43MjIyNTIsICdBdSBtaWxpZXUnXSxbNDUuMTgwNjU4LCA1LjcyMjI1NywgJ0xhIHBp6GNlIGRcJ2FwcuhzJ10sWzQ1LjE4MDYzOSwgNS43MjIyNzUsICdGYWl0ZXMgZGVtaS10b3VyJ10sWzQ1LjE4MDYwOCwgNS43MjIyODAsICdPdXZyZXogdm9pciddLFs0NS4xODA1OTQsIDUuNzIyMjkzLCAnVW4gYXV0cmUgcGxhY2FyZCA/J10sWzQ1LjE4MDU5NCwgNS43MjIyOTMsICdSZXRvdXIgdmVycyBsXCdlbnRy6WUnXSxbNDUuMTgwNTk0LCA1LjcyMjI5MywgJ0xhIGdyYW5kZSBwaehjZS4uLiddXQ==
 
